239adc42a2da7c3f165cdd5174bfbd63a70e97ed
NarrowPathDev <29716092+NarrowPathDev@users.noreply.github.com>
2026-09-17 18:09:53 -0400

    Remove temporary admin bootstrap

--- build.sh
+++ build.sh
@@ -9,37 +9,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate --noinput
-
-if [ -n "${DJANGO_SUPERUSER_USERNAME:-}" ] && \
-   [ -n "${DJANGO_SUPERUSER_PASSWORD:-}" ]; then
-
-    python manage.py shell <<'PY'
-import os
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-username = os.environ["DJANGO_SUPERUSER_USERNAME"]
-password = os.environ["DJANGO_SUPERUSER_PASSWORD"]
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
-
-user, created = User.objects.get_or_create(
-    username=username,
-    defaults={
-        "email": email,
-        "is_staff": True,
-        "is_superuser": True,
-    },
-)
-
-user.email = email
-user.is_staff = True
-user.is_superuser = True
-user.set_password(password)
-user.save()
-
-print("Production admin account configured.")
-PY
-
-fi
